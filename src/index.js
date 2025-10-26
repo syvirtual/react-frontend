b6bb806ce6c4faf989ad7517efa6e4f0adf3704e
syvirtual <sy_virtualpn@protonmail.com>
2025-10-25 18:27:40 -0700

    Create index.js
    React  js file

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fastapi-backend.onrender.com/") // replace with your actual backend URL
+      .then(res => res.json())
+      .then(json => setMessage(json.message));
+
+    fetch("https://fastapi-backend.onrender.com/api/data")
+      .then(res => res.json())
+      .then(json => setData(json.data));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>{message}</h1>
+      <h2>Data from Backend:</h2>
+      <ul>
+        {data.map((n, i) => (
+          <li key={i}>{n}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<App />);
